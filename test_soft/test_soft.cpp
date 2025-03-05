4a115bdf021063eb2d61af6b9da517799adfde70
Max <maximbacksim123@gmail.com>
2025-03-05 20:23:05 +0200

    Am adaugat functia salut()

--- test_soft/test_soft.cpp
+++ test_soft/test_soft.cpp
@@ -5,10 +5,14 @@ int test(int a, int b) {
     return a + b;
 }
 
-
+void salut() {
+    cout << "Ce mai faci?";
+}
 int main() {
     int x,y;
     cin >> x >> y;
     cout << test(x,y) << endl;
+
+    salut();
     return 0;
 }
